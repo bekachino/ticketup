0e90f5cf500784e2da2e2b79d064088fee1e91c3
Bektur Razzakov <brzzkv@gmail.com>
2024-08-08 14:41:46 +0600

    remove unused variables

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { useLocation } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { RU_PATHNAMES } from "../../constants";
 import './toolbar.css';
 
 const AppToolbar = () => {
-  const dispatch = useAppDispatch();
-  const [anchorEl, setAnchorEl] = useState(false);
   const location = useLocation();
-  const userToken = useAppSelector((state) => state.userState.user);
   
   return (
     <Box sx={{ flexGrow: 1 }}
