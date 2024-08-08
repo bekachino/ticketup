1412c2e1558c5f5ec30dc92c5374b6e3646ae6c2
Bektur Razzakov <brzzkv@gmail.com>
2024-08-08 11:42:52 +0600

    store and hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './app/store';
+import { Provider } from 'react-redux';
+import { addInterceptors } from './axiosApi';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+addInterceptors(store);
+
+root.render(<ThemeProvider theme={darkTheme}>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  </PersistGate>
+</ThemeProvider>);
