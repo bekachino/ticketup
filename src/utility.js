3b0cbe43abe22f9f01374b65e394550fbe065009
Bektur Razzakov <brzzkv@gmail.com>
2024-10-27 19:04:29 +0600

    update utility.js

--- src/utility.js
+++ src/utility.js
@@ -9,11 +9,13 @@ export const checkVersionAndClearCache = () => {
         : `${c.trim()}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
     });
 
-    caches.keys().then((names) => {
-      names.forEach((name) => {
-        caches.delete(name).then((_) => {});
+    if ('caches' in window) {
+      caches.keys().then((names) => {
+        names.forEach((name) => {
+          caches.delete(name).then((_) => {});
+        });
       });
-    });
+    }
 
     localStorage.setItem('TicketUpVersion', currentVersion);
 
